da09b3c6e93dd693a74ddf1e2b8cb3121d9e2d7d
kimhy <kimhy2745@gmail.com>
2024-11-14 14:26:26 +0900

    Initial Commit

---
+++ Coco/CocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoApp.swift
+//  Coco
+//
+//  Created by 김하연 on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CocoTests/CocoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CocoTests.swift
+//  CocoTests
+//
+//  Created by 김하연 on 11/14/24.
+//
+
+import Testing
+@testable import Coco
+
+struct CocoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
